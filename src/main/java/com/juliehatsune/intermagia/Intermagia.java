392f6094d182e7cf01ad3ebe57a08b0c20a06d4f
Juliee <juliehoelzl0503@gmail.com>
2025-11-28 20:29:45 +0100

    >:3

---
+++ src/main/java/com/juliehatsune/intermagia/Intermagia.java
@@ -0,0 +1,15 @@
+package com.juliehatsune.intermagia;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Intermagia implements ModInitializer {
+	public static final String MOD_ID = "intermagia";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/juliehatsune/intermagia/IntermagiaClient.java
@@ -0,0 +1,11 @@
+package com.juliehatsune.intermagia;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IntermagiaClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
